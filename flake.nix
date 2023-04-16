eeaf22af2241f6e73ebe7ca94ab48655d7d75c63
zgl@Ubuntu <zglinus@163.com>
2023-04-16 21:19:46 +0800

    Enable deploy

--- flake.nix
+++ flake.nix
@@ -39,34 +39,32 @@
       };
 
 
-      # deploy = {
-      #   sshUser = "nix"; # SSH 登录用户名
-      #   user = "root"; # 远程操作的用户
+      deploy = {
+        sshUser = "root"; # SSH 登录用户名
+        user = "root"; # 远程操作的用户
 
         # 部署失败自动回滚，建议关闭
         # 因为 NixOS（尤其是 Unstable 分支）部署不太稳定，有时需要部署两次才成功
         # 如果自动回滚了，反而适得其反，导致连续部署失败
-        # autoRollback = false;
+        autoRollback = false;
 
         # 断网自动回滚，建议关闭
         # 在你配置防火墙或 IP 出错把网络干掉时，自动回滚，这样你就不用去主机商控制面板连 VNC 或 IPMI 了
         # 但如果你就是在调整防火墙或者 IP 配置，会有当时断网、但重启机器就可以应用新配置恢复正常的情况
         # 自动回滚反而适得其反，因此建议关闭
-        # magicRollback = false;
+        magicRollback = true;
         #tempPath = /home/nix/.deploy-rs;
 
-        # nodes = {
-        #   "nixos" = {
-        #     # 目标机器的地址，IP 或域名或 .ssh/config 中配置的别名均可
-        #     hostname = "192.168.123.12";
-        #     profiles.hello = {
-        #       profilePath = "/nix/var/nix/profiles/per-user/nix/system";
-
-        #       # 调用上面的 nixosConfigurations."nixos"
-        #       path = deploy-rs.lib.x86_64-linux.setActivate nixpkgs.legacyPackages.x86_64-linux.hello "./bin/hello";
-        #     };
-        #   };
-        # };
-      # };
+        nodes = {
+          "nixos" = {
+            # 目标机器的地址，IP 或域名或 .ssh/config 中配置的别名均可
+            hostname = "nix";
+            profiles.system = {
+            # 调用上面的 nixosConfigurations."nixos"
+            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations."nixos";
+          };
+          };
+        };
+      };
     };
 }
