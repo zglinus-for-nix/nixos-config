b73eb57cb4763a1a414f4ef905c7e20a369b7577
zgl@Ubuntu <zglinus@163.com>
2023-04-17 12:24:26 +0800

    Fix path error

--- autodeploy.sh
+++ autodeploy.sh
@@ -22,8 +22,8 @@ do
         linenum=`cat /donotpush/logfile | wc -l`
         linenum_l3=`expr $linenum - 3`
         linenum_l2=`expr $linenum - 2`
-        OUTPUT1=$(sed -n ''"$linenum_l3"'p' /donotpush/logfile|base64)
-        OUTPUT2=$(sed -n ''"$linenum_l2"'p' /donotpush/logfile|base64)
+        OUTPUT1=$(sed -n ''"$linenum_l3"'p' ./donotpush/logfile|base64)
+        OUTPUT2=$(sed -n ''"$linenum_l2"'p' ./donotpush/logfile|base64)
         OUTPUT3=$(cat ./donotpush/logfile|tail -n1|base64)
         python dingdingbot.py $COMMIT $MESSAGE $OUTPUT1 $OUTPUT2 $OUTPUT3
         echo "level 4" > /proc/acpi/ibm/fan
