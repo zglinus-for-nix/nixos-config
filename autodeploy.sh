136771c09f7aeb60ea3fee21a86391c31bc7506c
zgl@Ubuntu <zglinus@163.com>
2023-04-17 12:54:12 +0800

    Feature fixed

--- autodeploy.sh
+++ autodeploy.sh
@@ -17,14 +17,15 @@ do
         MESSAGE=$(curl https://api.github.com/repos/zglinus-for-nix/nixos-config/commits | jq -r ".[0].commit.message"|base64)
         git pull origin
         echo "level auto" > /proc/acpi/ibm/fan
-        nix run github:serokell/deploy-rs -- -s . -- --print-build-logs > ./donotpush/logfile 2>&1
-        sed -i -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2}(;[0-9]{1,2})?)?)?[m|K]//g" ./donotpush/logfile
+        #nix run github:serokell/deploy-rs -- -s . -- --print-build-logs > ./donotpush/logfile 2>&1
+        #sed -i -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2}(;[0-9]{1,2})?)?)?[m|K]//g" ./donotpush/logfile
         linenum=`cat ./donotpush/logfile | wc -l`
         linenum_l3=`expr $linenum - 2`
         linenum_l2=`expr $linenum - 1`
-        OUTPUT1=$(sed -n ''"$linenum_l3"'p' ./donotpush/logfile|base64)
-        OUTPUT2=$(sed -n ''"$linenum_l2"'p' ./donotpush/logfile|base64)
-        OUTPUT3=$(sed -n ''"$linenum"'p' ./donotpush/logfile|base64)
+        OUTPUT1=$(sed -n ''"$linenum_l3"'p' ./donotpush/logfile|base64 -w 0)
+        OUTPUT2=$(sed -n ''"$linenum_l2"'p' ./donotpush/logfile|base64 -w 0)
+        OUTPUT3=$(sed -n ''"$linenum"'p' ./donotpush/logfile|base64 -w 0)
+        echo $OUTPUT1 $OUTPUT2 $OUTPUT3
         python dingdingbot.py $COMMIT $MESSAGE $OUTPUT1 $OUTPUT2 $OUTPUT3
         echo "level 4" > /proc/acpi/ibm/fan
         echo $COMMIT > donotpush/rev
