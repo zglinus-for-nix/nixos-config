bed5032b66f8a4d8cd6423b9aeef7543ba18b21a
zgl@Ubuntu <zglinus@163.com>
2023-04-21 22:03:58 +0800

    :bug: fix group bug 2

--- server/code.nix
+++ server/code.nix
@@ -2,19 +2,17 @@
 
 {
   users.users.code = {
-    group = "code";
     password = "Z@gl2001code";
     isNormalUser = true;
     description = "vscode";
     createHome = true;
     home = "/home/code";
     homeMode = "777";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" "code-server"];
   };
 
   services.code-server = { 
     enable = true;
     user = "code";
-    group = "code";
   };
 }
