9836c9d32fbe3a6d7be5dc5d67551489d58edca6
zgl@Ubuntu <zglinus@163.com>
2023-04-21 21:56:34 +0800

    :bug: too many numbers

--- load.py
+++ load.py
@@ -5,7 +5,7 @@ load2 = float(sys.argv[2])
 time = int(sys.argv[3])
 
 if time <= 900:
-    load = (load2*900 - load1*(900-time))/time
+    load = round((load2*900 - load1*(900-time))/time,2)
 else:
     load = load2
 
