a470764b0335182d92aee31a4f8f1abf61004611
zglinus <zglinus@163.com>
2023-04-25 00:00:47 +0800

    :rocket: remove deepin-wine

--- configuration.nix
+++ configuration.nix
@@ -229,7 +229,7 @@ in
     (pkgs.callPackage ./user/ffmpeg-codecs.nix { })
     (pkgs.callPackage ./user/tigervnc.nix { })
     #(pkgs.callPackage ./user/qq.nix { })
-    (pkgs.callPackage ./user/wine/default.nix { })
+    #(pkgs.callPackage ./user/wine/default.nix { })
     #(pkgs.callPackage ./user/wine/deepin-wine-helper.nix { })
     #(pkgs.callPackage ./user/xx.nix { })
   ] ;
