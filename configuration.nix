4220f86261480959bf3a9baef0aa3a480bac0cac
zglinus <zglinus@163.com>
2023-04-24 22:21:44 +0800

    :rocket: install freetype

--- configuration.nix
+++ configuration.nix
@@ -181,7 +181,7 @@ in
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     vscode
-    python2Full
+    python2Full freetype
     nvidia-offload
     zsh
     git
