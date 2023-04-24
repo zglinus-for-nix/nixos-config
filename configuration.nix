d42c01d2200d7170fce72437eeb4253865f98cbd
zglinus <zglinus@163.com>
2023-04-24 23:45:00 +0800

    :coffin:remove deepin-wine & QQ

--- configuration.nix
+++ configuration.nix
@@ -228,9 +228,9 @@ in
     (pkgs.callPackage ./user/vivaldi-widevine.nix { })
     (pkgs.callPackage ./user/ffmpeg-codecs.nix { })
     (pkgs.callPackage ./user/tigervnc.nix { })
-    (pkgs.callPackage ./user/qq.nix { })
-    (pkgs.callPackage ./user/wine/default.nix { })
-    (pkgs.callPackage ./user/wine/deepin-wine-helper.nix { })
+    #(pkgs.callPackage ./user/qq.nix { })
+    #(pkgs.callPackage ./user/wine/default.nix { })
+    #(pkgs.callPackage ./user/wine/deepin-wine-helper.nix { })
     #(pkgs.callPackage ./user/xx.nix { })
   ] ;
 
